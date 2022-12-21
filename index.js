400d69459ee59b8463d2d3c46fae1e63f91529e9
Valentina Ivanova <iv.valya.vi85@gmail.com>
2022-12-21 16:11:39 +0300

    feat: create mobile basket

--- index.js
+++ index.js
@@ -4,3 +4,15 @@ menu.addEventListener('scroll', function(e) {
     console.log(e.target);
     document.querySelector('.menu__mobile_nav').style.backgroundColor = "rgb(255, 255, 255, 0.7)";
   });
+
+let count = document.querySelector(".input_text");
+console.log(count);
+let minus = document.getElementById("left");
+let plus = document.getElementById("right");
+minus.addEventListener("click", () => {
+  console.log(count.value);
+  if (count.value > 1) {count.value--};
+});
+plus.addEventListener("click", () => {
+  if (count.value < 999) {count.value++};
+});
