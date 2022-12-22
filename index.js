849a1dc75f85b2cc21f34dd216d627d70ec2bb29
Valentina Ivanova <iv.valya.vi85@gmail.com>
2022-12-22 15:25:03 +0300

    faet: burger menu swipe

--- index.js
+++ index.js
@@ -12,13 +12,29 @@
     document.querySelector(".mobileBasket").style.height = (window.innerHeight) +"px";
     console.log(window.innerHeight-357);
   document.querySelector(".mobileBasket__menu").style.height = (window.innerHeight-360) + "px" };
-let menu = document.querySelector(".menu");
 */
+  const menu = document.querySelector(".menu");
+  const menu__h2 = document.querySelector(".menu__h2");
+  const burger = document.querySelector(".burgerMenu");
+
+  //--------------Добавление/снятие прозрачности у мобильного меню-------------------//
+
 menu.addEventListener('scroll', function(e) {
     console.log(e.target);
-    document.querySelector('.menu__mobile_nav').style.backgroundColor = "rgb(255, 255, 255, 0.7)";
+    const h2top = menu__h2.getBoundingClientRect().top
+    console.log(h2top);
+    if (h2top>=149) {
+      document.querySelector('.menu__mobile_nav').style.backgroundColor = "white";
+    }
+    else {
+      document.querySelector('.menu__mobile_nav').style.backgroundColor = "rgb(255, 255, 255, 0.7)";
+    }
   });
 
+
+
+
+//--------------------  Плюс/минус в меню-------------------//
 let count = document.querySelector(".input_text");
 console.log(count);
 let minus = document.getElementById("left");
@@ -30,3 +46,9 @@ minus.addEventListener("click", () => {
 plus.addEventListener("click", () => {
   if (count.value < 999) {count.value++};
 });
+
+//----------------------Вызов бургер-меню-----------------//
+/*burger.addEventListener("click", () => {
+  console.log("here");
+  document.querySelector(".mobile").classList.toggle("hidden");
+})*/
