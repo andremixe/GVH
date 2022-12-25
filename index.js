a480749299004870c26e21b443a91e05fde26ed4
Valentina Ivanova <iv.valya.vi85@gmail.com>
2022-12-25 12:36:56 +0300

    fix: footer

--- index.js
+++ index.js
@@ -17,7 +17,11 @@ let vh = window.innerHeight * 0.01;
 // Then we set the value in the --vh custom property to the root of the document
 document.documentElement.style.setProperty('--vh', `${vh}px`);
 
-
+window.addEventListener('resize', () => {
+  // We execute the same script as before
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
 
 const menu = document.querySelector(".menu");
 const main = document.querySelector(".main");
