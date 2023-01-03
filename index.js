4adb796529d55ef69bde26618a2fc8ab0654b924
Valentina Ivanova <iv.valya.vi85@gmail.com>
2023-01-03 16:19:06 +0300

    fix: overflow hidden for burger menu

--- index.js
+++ index.js
@@ -311,6 +311,27 @@ window.addEventListener("scroll", function () {
     }
   }
 });
+
+/*--------------------------Choose delivery/takeaway--------------------------*/
+const btnDelivery = document.querySelectorAll(".mobileBasket__delivery__item");
+for (let t = 0;  t < (btnDelivery.length-1); t++) {
+  btnDelivery[t].addEventListener("click", (e) => {
+    let currentDelivery= e.currentTarget.closest(".mobileBasket__delivery__item");
+    currentDelivery.classList.toggle("mobileBasket__delivery_active");
+    console.log(e.target);
+    console.log(btnDelivery.length);
+    btnDelivery[t].classList.toggle("mobileBasket__delivery_active");
+    if (t === (btnDelivery.length-1)) {
+      btnDelivery[t-1].classList.toggle("mobileBasket__delivery_active");
+    }
+    else {
+      btnDelivery[t+1].classList.toggle("mobileBasket__delivery_active");
+    }
+  })
+}
+
+
+
 /*---------------All parameters------------------------
 
 totalCount  //Общее количество заказанных позиций*/
